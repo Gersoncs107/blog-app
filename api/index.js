e82a16422b312f8d3e261c98b25f4c513e24eeab
Gersoncs107 <gerson.silva107@gmail.com>
2025-04-17 19:47:37 -0300

    blog-app

--- api/index.js
+++ api/index.js
@@ -4,3 +4,14 @@ require('dotenv').config()
 
 const app = express()
 const prisma = new PrismaClient()
+
+app.use(express.json())
+
+app.get('/', async (req, res) => {
+  res.send('Blog API')
+})
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
+})
