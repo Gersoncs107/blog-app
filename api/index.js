033ca2f09b94685dcb302a17c8a11a19b9193924
Gersoncs107 <gerson.silva107@gmail.com>
2025-04-17 23:34:46 -0300

    blog-app

--- api/index.js
+++ api/index.js
@@ -1,11 +1,18 @@
 const express = require('express')
 const {PrismaClient} = require('@prisma/client')
 require('dotenv').config()
-
 const app = express()
 const prisma = new PrismaClient()
 
+const postRoutes = require('./routes/posts')
+const commentRoutes = require('./routes/comments')
+const userRoutes = require('./routes/users')
+
 app.use(express.json())
+
+app.use('/posts', postRoutes)
+app.use('/comments', commentRoutes)
+app.use('/users', userRoutes)
 
 app.get('/', async (req, res) => {
   res.send('Blog API')
