7217f786bae269956a18b5bf954354268ccd70b3
Gersoncs107 <gerson.silva107@gmail.com>
2025-04-17 22:55:31 -0300

    blog-app

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -7,3 +7,5 @@ router.get('/:id', postController.getPostById)
 router.post('/', postController.createPost)
 router.put('/:id', postController.updatePost)
 router.delete('/:id', postController.deletePost)
+
+module.exports = router
