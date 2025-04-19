d3b820b8f18f7aa7279e3774e01b2fc3e0dd5f1f
Gersoncs107 <gerson.silva107@gmail.com>
2025-04-18 21:58:53 -0300

    blog-app

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const postController = require('../controllers/postController');
+const {
+    getPosts,
+    getPostById,
+    createPost,
+    updatePost,
+    deletePost,
+} = require('../controllers/postController');
 
-router.get('/', postController.getAllPosts);
-router.get('/:id', postController.getPostById);
-router.post('/', postController.createPost);
-router.put('/:id', postController.updatePost);
-router.delete('/:id', postController.deletePost);
+router.get('/', getPosts);
+router.get('/:id', getPostById);
+router.post('/', createPost);
+router.put('/:id', updatePost);
+router.delete('/:id', deletePost);
 
 module.exports = router;
