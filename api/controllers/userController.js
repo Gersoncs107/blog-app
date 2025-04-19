a01e942e3e029ecb1da027ab31fc1884dc340539
Gersoncs107 <gerson.silva107@gmail.com>
2025-04-18 22:34:39 -0300

    blog-app

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -4,12 +4,18 @@ const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 
 exports.login = async (req, res) => {
-    const {email, password} = req.body
+    const { email, password } = req.body;
     try {
-        
+      const user = await prisma.user.findUnique({ where: { email } });
+      if (!user || !(await bcrypt.compare(password, user.password))) {
+        return res.status(401).json({ error: 'Credenciais inválidas' });
+      }
+      const token = jwt.sign({ userId: user.id, role: user.role }, process.env.JWT_SECRET, {
+        expiresIn: '1h',
+      });
+      res.json({ token });
     } catch (error) {
-        res.status(401).json({ error: 'Erro ao fazer login'})
-        
+      res.status(500).json({ error: 'Erro ao fazer login' });
     }
-}
+  };
 
