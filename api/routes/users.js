4bd2326155458448b6271cf301d0c21c466228b2
Gersoncs107 <gerson.silva107@gmail.com>
2025-04-19 22:38:03 -0300

    blog-app

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,10 +1,11 @@
 // filepath: c:\Users\Gerson\Desktop\TheOdinProject\blog-app\api\routes\users.js
 const express = require('express');
 const router = express.Router();
-const {registerUser} = require('../controllers/userController');
+const {registerUser,login, getAllUsers} = require('../controllers/userController');
 
 // Example placeholder handlers
-router.get('/', (req, res) => res.send('Users route'));
+router.get('/', (req, res) => getAllUsers);
 router.post('/register', registerUser);
+router.post('/login', login);
 
 module.exports = router;
