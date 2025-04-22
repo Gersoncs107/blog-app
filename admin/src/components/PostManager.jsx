9a36fda8fc16fe4f49af72e7f7abe32add1f3623
Gersoncs107 <gerson.silva107@gmail.com>
2025-04-21 22:19:59 -0300

    blog-app

--- admin/src/components/PostManager.jsx
+++ admin/src/components/PostManager.jsx
@@ -2,4 +2,13 @@ import { useEffect, useState } from "react";
 
 function PostManager(){
     const [posts, setPosts] = useState([])
+
+    useEffect(() =>{
+        const token = localStorage.getItem('token')
+        fetch('http://localhost:3000/posts', {
+            headers: {Authorization: `Bearer ${token}`}
+        })
+        .then((res) => res.json())
+        .then((data) => setPosts(data))
+    }, [])
 }
