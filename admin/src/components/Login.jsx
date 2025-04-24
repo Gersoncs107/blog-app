cc36fef9c3a3dbcc3acfb644947a8a29334906de
Gersoncs107 <gerson.silva107@gmail.com>
2025-04-24 19:58:47 -0300

    blog-app

--- admin/src/components/Login.jsx
+++ admin/src/components/Login.jsx
@@ -5,4 +5,16 @@ function Login({setToken}){
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     const [error, setError] = useState('')
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        try {
+            const response = await axios.post(`${process.env.REACT_APP_API_URL}/users/login`, {
+                email,
+                password
+            })
+        } catch (error) {
+            setError('Invalid email or password')
+        }
+    }
 }
