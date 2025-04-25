c376e70d9c1a5d47fa16bcf88d772e3b416a02f5
Gersoncs107 <gerson.silva107@gmail.com>
2025-04-24 22:15:59 -0300

    blog-app

--- admin/src/components/PostList.jsx
+++ admin/src/components/PostList.jsx
@@ -5,4 +5,19 @@ import PostForm from './PostForm'
 function PostList({token}){
     const [posts, setPosts] = useState([])
     const [editingPost, setEditingPost] = useState(null)
+
+    useEffect(()=> {
+        fetchPosts()
+    }, [])
+
+    const fetchPosts = async () => {
+        try {
+            const response = await axios.get(`${process.env.REACT_APP_API_URL}/posts`, {
+                headers: {Authorization: `Bearer ${token}`}
+            })
+            setPosts(response.data)
+        } catch (error) {
+            
+        }
+    }
 }
