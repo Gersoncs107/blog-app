5184ec7b06423d054a610f13d8dfbf4067f3f76e
Gersoncs107 <gerson.silva107@gmail.com>
2025-04-24 22:04:23 -0300

    blog-app

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -7,20 +7,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Gerenciamento do Blog</h1>
+        {token && <button onClick={handleLogout}>Sair</button>}
       </header>
+      {token ? <PostList token={token} /> : <Login setToken={setToken} />}
     </div>
   );
 }
