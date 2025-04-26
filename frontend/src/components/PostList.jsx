cf2cd09631d88ca3dbcb8debf948c9a4370cd41f
Gersoncs107 <gerson.silva107@gmail.com>
2025-04-26 19:58:22 -0300

    blog-app

--- frontend/src/components/PostList.jsx
+++ frontend/src/components/PostList.jsx
@@ -28,29 +28,3 @@ function PostList() {
 }
 
 export default PostList;
-
-// import {useEfect, useState} from 'react';
-
-// function PostList(){
-//     const [posts, setPosts] = useState([])
-
-//     useEfect(() => {
-//         fetch('http://localhost:3000/posts')
-//         .then((res) => res.json())
-//         .then((data) => setPosts(data))
-//     }, [])
-
-//     return(
-//         <div>
-//             <h1>Blog</h1>
-//         {posts.map((post) => (
-//         <div key={post.id}>
-//           <h2>{post.title}</h2>
-//           <p>{post.content}</p>
-//         </div>
-//       ))}
-//         </div>
-//     )
-// }
-
-// export default PostList
