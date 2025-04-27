4b18a9f7ccacbd25cdf341891cd0d7d9e696f71a
Gersoncs107 <gerson.silva107@gmail.com>
2025-04-26 22:06:52 -0300

    blog-app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import PostList from './components/PostList';
+import PostDetail from './components/PostDetail';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header>
+          <h1>Blog Público</h1>
+        </header>
+        <Routes>
+          <Route path="/" element={<PostList />} />
+          <Route path="/posts/:id" element={<PostDetail />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
