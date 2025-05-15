a5cc3cbf63333ed04d4ed7fdb66ac7f3b0fe5706
Gersoncs107 <gerson.silva107@gmail.com>
2025-05-15 19:42:21 -0300

    blog-app

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -7,11 +7,15 @@ const {
     updatePost,
     deletePost,
 } = require('../controllers/postController');
+const { createComment } = require('../controllers/commentController');
 
 router.get('/', getPosts);
 router.get('/:id', getPostById);
 router.post('/', createPost);
 router.put('/:id', updatePost);
 router.delete('/:id', deletePost);
+
+// Add this for nested comments
+router.post('/:postId/comments', createComment);
 
 module.exports = router;
